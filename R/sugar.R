ace5c042bd07ef1c899105c4b77b7c4c81ca09b2
highandmighty <arseny@softculture.cc>
2019-12-13 02:33:38 +0300

    airtable_caller fixed

--- R/sugar.R
+++ R/sugar.R
@@ -4,7 +4,7 @@
 
 #' Airtable Caller
 #'
-#' Parse data from Airtable via API as JSON.
+#' Fetches data from a single call to Airtable API into JSON.
 #'
 #' @inheritParams fetch_airtable
 #' @return JSON data if request was successful.
@@ -12,7 +12,7 @@
 #' @examples
 #' json = call_airtable(AT_BASE, AT_TABLE, AT_TOKEN, query=list("filterByFormula"="(appsboard=1)"))
 call_airtable = function(base, table, token, query) {
-  url = paste0(base, table)
+  url = paste("https://api.airtable.com/v0", base, table, sep="/")
   r = httr::GET(url, httr::add_headers(Authorization=paste0("Bearer ", token)), query=query)
   if (httr::status_code(r) == 429) {
     print("Airtable API limit reached, 30 seconds break")
@@ -24,10 +24,10 @@ call_airtable = function(base, table, token, query) {
 
 #' Airtable Parser
 #'
-#' Parse data from Airtable via API as dataframe object.
+#' Fetches data from Airtable API into dataframe.
 #'
-#' @param base Airtable database URL e.g. `"https://api.airtable.com/v0/app5gby0DG3pF9KbB/"`
-#' @param table Airtable tablename e.g. `"table1"`
+#' @param base Airtable base ID e.g. `"app5gby0DG3pF9KbB"`
+#' @param table Airtable tablename e.g. `"tablename"`
 #' @param token Airtable API token
 #' @param query A list of query parameters e.g. `list("filterByFormula"="(appsboard=1)")`
 #' @return A dataframe with Airtable data.
@@ -52,7 +52,7 @@ fetch_airtable = function(..., query=list()) {
 
 #' Feather from URL Fetcher
 #'
-#' Fetch Feather file from URL to dataframe.
+#' Fetches Feather file from URL into dataframe.
 #'
 #' @param url A direct URL of Feather file
 #' @return A dataframe with Feather file's data.
